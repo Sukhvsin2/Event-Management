857744fe7801a8b66e5b446c4edd4368cb1c8271
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-03-10 15:52:10 -0600

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <Header/>
+    <Calendar/>
+  </v-app>
+</template>
+
+<script>
+import Header from "../components/Header"
+import Calendar from "../components/Calendar"
+
+export default {
+  name:"Home",
+  components: {
+    Header,
+    Calendar
+  }
+}
+</script>
